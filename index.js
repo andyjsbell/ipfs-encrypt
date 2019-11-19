d34cab718d20d334db7074dd96e5535d463fe27c
Andy Bell <andy.bell@displaynote.com>
2019-11-19 17:24:54 +0100

    Return object

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 async function getPublicKeyForFirstAccount() {
     let hashedMsg = web3.utils.sha3('test');
     let accounts = await web3.eth.getAccounts();
-    
-    let signature = await web3.eth.sign(hashedMsg, accounts[0]);
+    let account = accounts[0];
+
+    let signature = await web3.eth.sign(hashedMsg, account);
     let sig = Util.fromRpcSig(signature);
 
     let pubKeyBuffer = Util.ecrecover(Util.toBuffer(hashedMsg), sig.v, sig.r, sig.s);
     let pubKey = Util.bufferToHex(pubKeyBuffer);
 
-    return pubKey;
+    return {pubKey, account};
 }
   
 (async() => {
-    console.log("Public Key: ", await getPublicKeyForFirstAccount());
+    console.log(await getPublicKeyForFirstAccount());
 })();

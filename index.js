74a9e88025e156243a38fd7b117003e7bb3fadc5
Andy Bell <andy.bell@displaynote.com>
2019-11-19 17:21:48 +0100

    Go async

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ Web3 = require('web3');
 Util = require('ethereumjs-util');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-let hashedMsg = web3.utils.sha3('test');
-
-web3.eth.getAccounts().then(accounts => {
+async function getPublicKeyForFirstAccount() {
+    let hashedMsg = web3.utils.sha3('test');
+    let accounts = await web3.eth.getAccounts();
     
-    console.log("account:", accounts[0]);
+    let signature = await web3.eth.sign(hashedMsg, accounts[0]);
+    let sig = Util.fromRpcSig(signature);
 
-    web3.eth.sign(hashedMsg, accounts[0]).then(signature => {
-        
-        let sig = Util.fromRpcSig(signature);
-    
-        let pubKeyBuffer = Util.ecrecover(Util.toBuffer(hashedMsg), sig.v, sig.r, sig.s);
-        let pubKey = Util.bufferToHex(pubKeyBuffer);
-        
-        console.log("pubKey:", pubKey);
-    });
-});
+    let pubKeyBuffer = Util.ecrecover(Util.toBuffer(hashedMsg), sig.v, sig.r, sig.s);
+    let pubKey = Util.bufferToHex(pubKeyBuffer);
 
+    return pubKey;
+}
+  
+(async() => {
+    console.log("Public Key: ", await getPublicKeyForFirstAccount());
+})();

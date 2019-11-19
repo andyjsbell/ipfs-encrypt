4c2d881c857d7299000ffc5a942c3694b2b69304
Andy Bell <andy.bell@displaynote.com>
2019-11-19 17:23:27 +0100

    Add commentary

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ Web3 = require('web3');
 Util = require('ethereumjs-util');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
+// Public key from the first ethereum account
+// Sign a message and then we can get back the public key back
+
 async function getPublicKeyForFirstAccount() {
     let hashedMsg = web3.utils.sha3('test');
     let accounts = await web3.eth.getAccounts();
